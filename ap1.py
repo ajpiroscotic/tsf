369c6e65a0bdf2bfa95107ea080efadc6375e703
ajpiroscotic <31822666+ajpiroscotic@users.noreply.github.com>
2020-07-23 20:15:40 +0530

    Create ap1.py

---
+++ ap1.py
@@ -0,0 +1,65 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from fbprophet import Prophet
+from fbprophet.diagnostics import performance_metrics
+from fbprophet.diagnostics import cross_validation
+from fbprophet.plot import plot_cross_validation_metric
+import base64
+
+st.title('Time Series Forecasting')
+
+
+
+
+df = st.file_uploader('Import the time series csv file here.', type='csv', encoding='auto')
+
+if df is not None:
+    data = pd.read_csv(df)
+    data['ds'] = pd.to_datetime(data['ds'],errors='coerce') 
+    
+    st.write(data)
+    
+    max_date = data['ds'].max()
+    #st.write(max_date)
+
+
+
+periods_input = st.number_input('How many periods would you like to forecast into the future?',
+min_value = 1, max_value = 365)
+
+if df is not None:
+    m = Prophet()
+    m.fit(data)
+
+
+"""
+if df is not None:
+    future = m.make_future_dataframe(periods=periods_input)
+    
+    forecast = m.predict(future)
+    fcst = forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']]
+
+    fcst_filtered =  fcst[fcst['ds'] > max_date]    
+    st.write(fcst_filtered)
+    
+    """
+    The next visual shows the actual (black dots) and predicted (blue line) values over time.
+    """
+    fig1 = m.plot(forecast)
+    st.write(fig1)
+
+    """
+    The next few visuals show a high level trend of predicted values, day of week trends, and yearly trends (if dataset covers multiple years). The blue shaded area represents upper and lower confidence intervals.
+    """
+    fig2 = m.plot_components(forecast)
+    st.write(fig2)
+
+
+
+if df is not None:
+    csv_exp = fcst_filtered.to_csv(index=False)
+    # When no file name is given, pandas returns the CSV as a string, nice.
+    b64 = base64.b64encode(csv_exp.encode()).decode()  # some strings <-> bytes conversions necessary here
+    href = f'<a href="data:file/csv;base64,{b64}">Download CSV File</a> (right-click and save as ** &lt;forecast_name&gt;.csv**)'
+    st.markdown(href, unsafe_allow_html=True)
